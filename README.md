c7f48cdc0c828a3c249b30883fa065bf9bfe8aba
Jake Wharton <jw@squareup.com>
2013-02-26 12:49:42 -0800

    Initial import from gist.

---
+++ README.md
@@ -0,0 +1,32 @@
+Maven + Google Play Services
+============================
+
+The recently released version of Google Play Services presents itself as a library
+project with string and attribute resources coupled with a `.jar` in the `libs/` folder
+that has been compiled against internal APIs.
+
+While this is convenient for users of IDEs or Ant, it presents a problem for those using
+proper build systems (e.g., Maven, Gradle) with dependency management. Inside the `.jar` there are a lot of classes
+which reference static attributes on the `com.google.android.gsm.R` class. This means that
+ the library must exist in a project which declares that package in its manifest
+(thus causing `aapt` to generate an `R.java` for it). While this makes sense, it presents
+a problem for users of artifact repositories.
+
+If you use Maven, you can do the following in order to depend on Google Play Services as
+a library project:
+
+ 1. Copy the `gms-mvn-install.sh` scripts into the `google-play-services_lib/` folder inside of your SDK.
+ 2. Execute `bash gms-mvn-install.sh` which will install the project into your local Maven repository.
+    If you want to deploy the artifacts to a remote repository then execute
+    `bash gms-mvn-install.sh repo-name repo-url`.
+
+Once completed you can depend on this library project with the following dependency declaration:
+
+```xml
+<dependency>
+  <groupId>com.google.android.gms</groupId>
+  <artifactId>google-play-services</artifactId>
+  <version>3</version>
+  <type>apklib</type>
+</dependency>
+```
---
+++ gms-mvn-install.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+set -e
+
+if [ $# -ne 0 -a $# -ne 2 ]; then
+	echo "Usage: $0 [repo-id repo-url]"
+	echo ""
+	echo "Installs Google Play Services to your local Maven repo or deploys it to a"
+	echo "remote repo if 'repo-id' and 'repo-url' are specified."
+	echo ""
+	exit 1
+fi
+
+REPO_ID="$1"
+REPO_URL="$2"
+
+cat > pom.xml <<EOF
+<?xml version="1.0" encoding="UTF-8"?>
+
+<project xmlns="http://maven.apache.org/POM/4.0.0"
+         xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+         xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+	<modelVersion>4.0.0</modelVersion>
+
+	<groupId>com.google.android.gms</groupId>
+	<artifactId>google-play-services</artifactId>
+	<version>5</version>
+	<packaging>apklib</packaging>
+
+	<dependencies>
+		<dependency>
+			<groupId>com.google.android</groupId>
+			<artifactId>android</artifactId>
+			<version>4.1.1.4</version>
+			<scope>provided</scope>
+		</dependency>
+		<dependency>
+			<groupId>com.google.android.gms</groupId>
+			<artifactId>google-play-services-jar</artifactId>
+			<version>5</version>
+		</dependency>
+	</dependencies>
+
+	<build>
+		<sourceDirectory>src</sourceDirectory>
+
+		<plugins>
+			<plugin>
+				<groupId>com.jayway.maven.plugins.android.generation2</groupId>
+				<artifactId>android-maven-plugin</artifactId>
+				<version>3.5.1</version>
+				<extensions>true</extensions>
+				<configuration>
+					<nativeLibrariesDirectory>ignored</nativeLibrariesDirectory>
+				</configuration>
+			</plugin>
+		</plugins>
+	</build>
+</project>
+EOF
+
+if [ -z "$REPO_ID" ]; then
+	# install locally
+
+	mvn org.apache.maven.plugins:maven-install-plugin:2.4:install-file \
+	  -DgroupId=com.google.android.gms \
+	  -DartifactId=google-play-services-jar \
+	  -Dversion=5 \
+	  -Dpackaging=jar \
+	  -Dfile=libs/google-play-services.jar
+
+	mvn clean install
+else
+	# deploy to remote
+
+	mvn org.apache.maven.plugins:maven-deploy-plugin:2.7:deploy-file \
+	  -DgroupId=com.google.android.gms \
+	  -DartifactId=google-play-services-jar \
+	  -Dversion=5 \
+	  -Dpackaging=jar \
+	  -Durl=$REPO_URL \
+	  -DrepositoryId=$REPO_ID \
+	  -Dfile=libs/google-play-services.jar
+
+	mvn clean deploy -DaltDeploymentRepository=$REPO_ID::default::$REPO_URL
+fi
